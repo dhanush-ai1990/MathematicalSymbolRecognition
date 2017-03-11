8909770c57dca201b570a41349a9fdb1c7df8488
Dhanush Dharmaretnam <dhanush987@gmail.com>
2017-03-10 18:00:39 -0800

    Pipeline for Training Established
    This is the first working Conv Net with two COV2D filters of size 48. A
    single layer Net with 128 neurons.

---
+++ Extract_img_pickle.py
@@ -0,0 +1,52 @@
+import numpy as np
+from PIL import Image
+import os, sys
+from sklearn.cross_validation import train_test_split
+import pickle
+
+dir1 = "/Users/Dhanush/Documents/Deeplearn/MathematicalSymbolRecognition/extracted_images"
+
+#Get all Directories, all 82 labels.
+label_dict = os.listdir(dir1)
+
+
+def load_image(infilename):
+    img = Image.open( infilename)
+    img.load()
+    data = np.asarray( img, dtype="float32" )
+    return data
+
+X = []
+y = []
+total_labels = 0
+for label in label_dict:
+	if label == ".DS_Store":
+		continue
+	dir2 = dir1 + "/" + str(label)
+	temp_dict = os.listdir(dir2)
+	count_per_label = 0
+	if len(temp_dict) < 200:
+		continue
+	total_labels +=1
+	for item in temp_dict:
+		y.append(label)
+		count_per_label+=1
+		img = dir1 + "/" + str(label)+"/"+ str(item)
+		X.append(load_image(img))
+	#print label + " " + str(count_per_label)
+print "Total labels considered for classification:  " + str(total_labels)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=436718)
+
+X_train = np.asarray(X_train)
+X_test = np.asarray(X_test)
+y_train = np.asarray(y_train)
+y_test = np.asarray(y_test)
+
+
+print X_train.shape
+print y_train.shape
+print X_test.shape
+print y_test.shape
+
+f = open("MathSymbols_train_test", "wb")
+pickle.dump((X_train, y_train, X_test, y_test),f)
